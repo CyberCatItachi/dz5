12860b3f665349045910b0aba97ec098c699e65e
Viktoria <michinyan1404@gmail.com>
2023-06-30 01:50:33 +0300

    Tasks

---
+++ task34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] GetRandomArray (int size, int minValue, int maxValue)
+{
+int[] result = new int[size];
+for (int i = 0; i < size; i++)
+{
+result[i] = new Random().Next(minValue, maxValue + 1);
+}
+
+return result;
+}
+Console.WriteLine("Введите длину массива:");
+int Length = int.Parse(Console.ReadLine());
+int[] array = GetRandomArray (Length, 99, 1000);
+int count = 0;
+for (int i = 0; i < Length; i++)
+{
+    if (array[i]%2==0)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine($"{String.Join(", ", array)}");
+Console.WriteLine($" Число четных чисел равно {count}");
---
+++ task38/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите элемент {i + 1} (через запятую)");
+    array[i] = double.Parse(Console.ReadLine());
+}
+double max = array.Max();
+double min = array.Min();
+Console.WriteLine($"Разница между максимальным и минимальным элементом равна {max - min}");
